b3b57bf39807d5af39713bf44a3a6b56fd49950e
Valmir <vmcfilho@gmail.com>
2024-06-11 21:25:41 -0300

    Create 18.sql

---
+++ 01-fundamentals/18.sql
@@ -0,0 +1,34 @@
+-- Utilizando o comando IF.
+
+SET SERVEROUTPUT ON
+
+-- Input de dados.
+ACCEPT pDepartment_id PROMPT 'Digite o ID do departamento: '
+
+DECLARE
+   vPercentual     NUMBER(3);
+   vDepartment_id  employees.employee_id%type := &pdepartment_id;
+BEGIN
+   IF vDepartment_id  =  80 -- Sales. 
+   THEN 
+       vPercentual := 10; 
+   ELSE 
+       IF  vDepartment_id  =  20 -- Marketing. 
+       THEN 
+           vPercentual := 15; 
+       ELSE
+           IF  vDepartment_id  =  60 -- IT. 
+           THEN 
+               vPercentual := 20;
+           ELSE 
+               vPercentual := 5;
+           END IF;
+       END IF;
+  END IF;
+  DBMS_OUTPUT.PUT_LINE('Id do Departamento: ' || vDepartment_id);   
+  DBMS_OUTPUT.PUT_LINE('Percentual: ' || vpercentual );   
+  UPDATE employees 
+  SET    salary = salary * (1 + vpercentual / 100)
+  WHERE department_id =  vDepartment_id;
+  COMMIT;
+END;
