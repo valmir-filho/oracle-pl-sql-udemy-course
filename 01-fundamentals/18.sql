96d6cc01315257ed3d206b578cb950687d78e65b
Valmir <vmcfilho@gmail.com>
2024-06-13 19:48:56 -0300

    Update 18.sql

--- 01-fundamentals/18.sql
+++ 01-fundamentals/18.sql
@@ -8,7 +8,7 @@ ACCEPT pDepartment_id PROMPT 'Digite o ID do departamento: '
 DECLARE
    vPercentual     NUMBER(3);
    -- O "&" serve para substituir o valor do ACCEPT na variável em questão.
-   vDepartment_id  employees.employee_id%type := &pdepartment_id;
+   vDepartment_id  employees.employee_id%type := &pDepartment_id;
 BEGIN
    IF vDepartment_id  =  80 -- Sales. 
    THEN 
@@ -28,10 +28,10 @@ BEGIN
   END IF;
   
   DBMS_OUTPUT.PUT_LINE('ID do Departamento: ' || vDepartment_id);   
-  DBMS_OUTPUT.PUT_LINE('Percentual: ' || vpercentual );   
+  DBMS_OUTPUT.PUT_LINE('Percentual: ' || vPercentual );   
   
   UPDATE employees 
-  SET    salary = salary * (1 + vpercentual / 100)
+  SET    salary = salary * (1 + vPercentual / 100)
   WHERE department_id =  vDepartment_id;
   COMMIT;
 END;
