eddd1bad930a6d593edb1570fa8a75a1f341500f
Valmir <vmcfilho@gmail.com>
2024-06-13 19:12:52 -0300

    Create 21.sql

---
+++ 01-fundamentals/21.sql
@@ -0,0 +1,16 @@
+-- Loop básico.
+
+SET SERVEROUTPUT ON
+
+ACCEPT pLimite PROMPT 'Digite o valor do limite: '
+
+DECLARE
+  vNumero  NUMBER(38) := 1;
+  vLimite  NUMBER(38) := &pLimite;
+BEGIN
+LOOP 
+  DBMS_OUTPUT.PUT_LINE('Número = ' || to_char(vNumero));
+  EXIT WHEN vNumero = vLimite;
+  vNumero := vNumero + 1; -- Iterador.
+END LOOP;
+END;
