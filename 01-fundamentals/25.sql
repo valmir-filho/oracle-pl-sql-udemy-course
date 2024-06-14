c6ce255c9211a76c4bf1a1062566ced969ba3a2e
Valmir <vmcfilho@gmail.com>
2024-06-13 21:17:28 -0300

    Update 25.sql

--- 01-fundamentals/25.sql
+++ 01-fundamentals/25.sql
@@ -25,3 +25,31 @@ DBMS_OUTPUT.PUT_LINE(employee_record.employee_id || ' - ' ||
                      employee_record.last_name || ' - ' || 
                      employee_record.phone_number);
 END;
+
+/*
+Obs.: Se a variável record foi declarada com o mesmo nome da tabela (usando o atributo %ROWTYPE), ela acaba recebendo
+todos os campos dela.
+*/
+
+-- Criando um PL/SQL Record utilizando atributo %ROWTYPE
+
+SET SERVEROUTPUT ON
+          
+SET VERIFY OFF
+          
+ACCEPT pEmployee_id PROMPT 'Digite o ID do empregado: '
+
+DECLARE
+employee_record   employees%rowtype;
+vEmployee_id      employees.employee_id%type := &pEmployee_id;
+BEGIN
+SELECT  * 
+INTO    employee_record
+FROM    employees
+WHERE   employee_id = vEmployee_id;
+DBMS_OUTPUT.PUT_LINE(employee_record.employee_id || ' - ' || 
+                     employee_record.first_name || ' - ' || 
+                     employee_record.last_name || ' - ' || 
+                     employee_record.phone_number || ' - ' ||
+                     employee_record.job_id);
+END;
